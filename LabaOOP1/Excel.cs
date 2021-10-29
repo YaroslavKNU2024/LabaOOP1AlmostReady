1305747f685fb825fd31e6e85df5e268cf0c1bbe
YaroslavKNU2024 <86433715+YaroslavKNU2024@users.noreply.github.com>
2021-10-29 12:58:28 +0300

    Add files via upload

---
+++ LabaOOP1/Excel.cs
@@ -0,0 +1,45 @@
+﻿using Antlr4.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabaOOP1
+{
+    public class Calculator
+    {
+        string s;
+        public Calculator(string exp) {
+            lexer = Lexer(new AntlrInputStream(exp));
+        }
+        public Calculator() {}
+        public TestExcelLexer lexer;
+        public  CommonTokenStream tokens;
+        public TestExcelParser parser;
+
+        public TestExcelLexer Lexer(AntlrInputStream stream) => new TestExcelLexer(stream);
+        public CommonTokenStream Tokenizer(TestExcelLexer lexer) => new CommonTokenStream(lexer);
+        public TestExcelParser Parser(CommonTokenStream token) => new TestExcelParser(token);
+
+        public double Evaluate(string expression)
+        {
+            var v = new AntlrInputStream(expression);
+            lexer = Lexer(v);
+            //var lexer = new TestExcelLexer(new AntlrInputStream(expression));
+
+            lexer.RemoveErrorListeners();
+            lexer.AddErrorListener(new ThrowExceptionErrorListener());
+
+            //var tokens = new CommonTokenStream(lexer);
+            //var parser = new TestExcelParser(tokens);
+            tokens = Tokenizer(lexer);
+            parser = Parser(tokens);
+
+            var tree = parser.compileUnit();
+            var visitor = new TestExcelVisitor();
+            double dd = visitor.Visit(tree);
+            return dd;
+        }
+    }
+}
